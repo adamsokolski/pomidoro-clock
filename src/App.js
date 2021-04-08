c16abf49090dafdb268ae2909aeda367b6452c79
idKrazu <idkrazu@gmail.com>
2021-04-08 23:55:14 +0200

    ✨ Add increment and decrement

--- src/App.js
+++ src/App.js
@@ -7,8 +7,42 @@ export default class App extends Component {
     super(props);
     this.state = {
       sessionLength: 25,
+      sessionTime: 1500,
       breakLength: 5,
+      breakTime: 300,
     };
+    this.sessionIncrement = this.sessionIncrement.bind(this);
+    this.sessionDecrement = this.sessionDecrement.bind(this);
+    this.breakIncrement = this.breakIncrement.bind(this);
+    this.breakDecrement = this.breakDecrement.bind(this);
+  }
+
+  sessionIncrement() {
+    this.setState((state) => ({
+      sessionLength: state.sessionLength + 1,
+      sessionTime: state.sessionTime + 60,
+    }));
+  }
+
+  sessionDecrement() {
+    this.setState((state) => ({
+      sessionLength: state.sessionLength - 1,
+      sessionTime: state.sessionTime - 60,
+    }));
+  }
+
+  breakIncrement() {
+    this.setState((state) => ({
+      breakLength: state.breakLength + 1,
+      breakTime: state.breakTime + 60,
+    }));
+  }
+
+  breakDecrement() {
+    this.setState((state) => ({
+      breakLength: state.breakLength - 1,
+      breakTime: state.breakTime - 60,
+    }));
   }
 
   render() {
@@ -24,12 +58,20 @@ export default class App extends Component {
         <div id="break-length">{breakLength}</div>
 
         <div id="session-label">Session Length</div>
-        <button id="session-decrement">-</button>
-        <button id="session-increment">+</button>
+        <button id="session-increment" onClick={this.sessionIncrement}>
+          +
+        </button>
+        <button id="session-decrement" onClick={this.sessionDecrement}>
+          -
+        </button>
 
         <div id="break-label">Break Length</div>
-        <button id="break-decrement">-</button>
-        <button id="break-increment">+</button>
+        <button id="break-increment" onClick={this.breakIncrement}>
+          +
+        </button>
+        <button id="break-decrement" onClick={this.breakDecrement}>
+          -
+        </button>
       </div>
     );
   }
