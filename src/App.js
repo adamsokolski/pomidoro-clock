2e22ad8c8ca4cac247bcadce2f5169e36b652360
idKrazu <idkrazu@gmail.com>
2021-04-09 23:25:30 +0200

    ✨ Add bell sound when timer finish ✨ Add function to reset button 🐛 Fix timer going under 00:00 🐛 Fix 60:00 displaying as 00:00

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import "./App.css";
-
 import React, { Component } from "react";
+import "./App.css";
+import bell from "./sounds/bell.mp3";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       sessionLength: 25,
-      sessionTime: 5,
+      sessionTime: 1500,
       breakLength: 5,
-      breakTime: 5,
+      breakTime: 300,
       timeLeft: "25:00",
       isRunning: false,
       intervalId: "",
@@ -20,6 +20,7 @@ export default class App extends Component {
     this.breakIncrement = this.breakIncrement.bind(this);
     this.breakDecrement = this.breakDecrement.bind(this);
     this.handleStart = this.handleStart.bind(this);
+    this.handleReset = this.handleReset.bind(this);
     this.secondsToTime = this.secondsToTime.bind(this);
     this.counting = this.counting.bind(this);
   }
@@ -69,6 +70,7 @@ export default class App extends Component {
         isRunning: false,
       }));
     } else {
+      this.counting();
       let intervalId = setInterval(this.counting, 1000);
       this.setState({
         intervalId: intervalId,
@@ -77,26 +79,57 @@ export default class App extends Component {
     }
   }
 
+  handleReset(e) {
+    clearInterval(this.state.intervalId);
+    this.setState({
+      sessionLength: 25,
+      sessionTime: 1500,
+      breakLength: 5,
+      breakTime: 300,
+      timeLeft: "25:00",
+      isRunning: false,
+      label: "Session",
+    });
+    let audio = e.target.nextElementSibling;
+    audio.currentTime = 0;
+    audio.pause();
+  }
+
   counting() {
     // Todo: check label and if current timer reaches 0 switch to another
 
-    if (this.state.sessionTime !== -1) {
+    if (this.state.label === "Session") {
       this.setState((state) => ({
         sessionTime: state.sessionTime - 1,
         timeLeft: this.secondsToTime(this.state.sessionTime),
         label: "Session",
       }));
-    } else {
+    } else if (this.state.label === "Break") {
       this.setState((state) => ({
         breakTime: state.breakTime - 1,
         timeLeft: this.secondsToTime(this.state.breakTime),
         label: "Break",
       }));
     }
+    if (this.state.sessionTime === -1) {
+      let audio = document.querySelector("#beep");
+      audio.play();
+      this.setState((state) => ({
+        sessionTime: state.sessionLength * 60,
+        label: "Break",
+      }));
+    } else if (this.state.breakTime === -1) {
+      let audio = document.querySelector("#beep");
+      audio.play();
+      this.setState((state) => ({
+        breakTime: state.breakLength * 60,
+        label: "Session",
+      }));
+    }
   }
 
   secondsToTime(e) {
-    let m = Math.floor((e % 3600) / 60)
+    let m = Math.floor(e / 60)
       .toString()
       .padStart(2, "0");
     let s = Math.floor(e % 60)
@@ -110,20 +143,29 @@ export default class App extends Component {
     const { sessionLength, breakLength, timeLeft, label } = this.state;
     return (
       <div className="App">
-        <div id="time-left">{timeLeft}</div>
-        <button id="start_stop" onClick={this.handleStart}>
-          Start
-        </button>
-        <button id="reset">Reset</button>
-
         <div id="timer-label">{label}</div>
-        <div id="session-length">{sessionLength}</div>
-        <div id="break-length">{breakLength}</div>
+
+        <div id="time-left">{timeLeft}</div>
+
+        <div>
+          <button id="start_stop" onClick={this.handleStart}>
+            Start
+          </button>
+          <button id="reset" onClick={this.handleReset}>
+            Reset
+          </button>
+          <audio src={bell} id="beep">
+            Your browser does not support the
+            <code>audio</code> element.
+          </audio>
+        </div>
 
         <div id="session-label">Session Length</div>
         <button id="session-increment" onClick={this.sessionIncrement}>
           +
         </button>
+        <div id="session-length">{sessionLength}</div>
+
         <button id="session-decrement" onClick={this.sessionDecrement}>
           -
         </button>
@@ -132,6 +174,8 @@ export default class App extends Component {
         <button id="break-increment" onClick={this.breakIncrement}>
           +
         </button>
+        <div id="break-length">{breakLength}</div>
+
         <button id="break-decrement" onClick={this.breakDecrement}>
           -
         </button>
